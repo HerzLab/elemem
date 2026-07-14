893975f3ae007a0011707d154c3231424973c1a8
James Bruska <bruskatech@protonmail.com>
2026-07-13 22:12:51 -0300

    First potentially working vesion of ExperPulse

--- src/ExperPulse.cpp
+++ src/ExperPulse.cpp
@@ -17,34 +17,60 @@ namespace CML {
   }
 
   void ExperPulse::SetStimProfile_Handler(const StimProfile& new_stim_profile) {
+    base_stim_profile = new_stim_profile;
+
+    if (base_stim_profile.size() == 0) {
+      Throw_RC_Error("ExperPulse requires at least one stimulation channel.");
+    }
+  }
+
+  uint64_t ExperPulse::CurrentPulseIntervalMs() const {
+    return pulse_intervals_ms[pulse_interval_idx];
+  }
+
+  uint32_t ExperPulse::CurrentPairFrequencyHz() const {
+    const uint64_t interval_ms = CurrentPulseIntervalMs();
+    return uint32_t((1000 + interval_ms/2) / interval_ms);
+  }
+
+  uint32_t ExperPulse::CurrentPairDurationUs() const {
+    const uint32_t frequency = CurrentPairFrequencyHz();
+    return (2000000 + frequency - 1) / frequency;
+  }
+
+  double ExperPulse::CurrentActualPulseIntervalMs() const {
+    return 1000.0 / CurrentPairFrequencyHz();
+  }
+
+  void ExperPulse::ConfigureCurrentInterval() {
     stim_profile.Clear();
 
-    for (size_t i=0; i<new_stim_profile.size(); i++) {
-      StimChannel chan = new_stim_profile[i];
-      chan.frequency = pair_frequency_hz;
-      chan.duration = pair_duration_us;
+    for (size_t i=0; i<base_stim_profile.size(); i++) {
+      StimChannel chan = base_stim_profile[i];
+      chan.frequency = CurrentPairFrequencyHz();
+      chan.duration = CurrentPairDurationUs();
       chan.burst_frac = 1;
       chan.burst_slow_freq = 0;
       stim_profile += chan;
-    }
-
-    if (stim_profile.size() == 0) {
-      Throw_RC_Error("ExperPulse requires at least one stimulation channel.");
     }
 
     hndl->stim_worker.ConfigureStimulation(stim_profile);
   }
 
   void ExperPulse::Start_Handler() {
-    if (stim_profile.size() == 0) {
+    if (base_stim_profile.size() == 0) {
       Throw_RC_Error("ExperPulse cannot start without a stimulation profile.");
     }
 
     BeAllocatedTimer();
     next_event_time = 0;
     pulse_pair_count = 0;
+    pulse_interval_idx = 0;
+    pulse_interval_trial = 0;
+    pulse_interval_repeat = 0;
+    ConfigureCurrentInterval();
 
-    if (!ConfirmWin("Pulse experiment will run for 5 min. 0 sec.",
+    if (!ConfirmWin("Pulse experiment will run for 6 min. 0 sec.",
           "Session Duration")) {
       hndl->StopExperiment();
       return;
@@ -53,7 +79,13 @@ namespace CML {
     exp_start = RC::Time::Get();
 
     JSONFile startlog = MakeResp("START");
-    startlog.Set(pulse_interval_ms, "data", "pulse_interval_ms");
+    RC::Data1D<uint64_t> pulse_intervals_log(pulse_intervals_ms.size());
+    for (size_t i=0; i<pulse_intervals_ms.size(); i++) {
+      pulse_intervals_log[i] = pulse_intervals_ms[i];
+    }
+    startlog.Set(pulse_intervals_log, "data", "pulse_intervals_ms");
+    startlog.Set(trials_per_interval, "data", "trials_per_interval");
+    startlog.Set(interval_repeats, "data", "interval_repeats");
     startlog.Set(pair_period_ms, "data", "pair_period_ms");
     startlog.Set(experiment_duration_ms, "data", "experiment_duration_ms");
     hndl->event_log.Log(startlog.Line());
@@ -79,7 +111,13 @@ namespace CML {
   void ExperPulse::DoPulsePair() {
     JSONFile pulse_event = MakeResp("PULSE_PAIR");
     pulse_event.Set(pulse_pair_count, "data", "pair_index");
-    pulse_event.Set(pulse_interval_ms, "data", "pulse_interval_ms");
+    pulse_event.Set(pulse_interval_repeat, "data", "interval_repeat");
+    pulse_event.Set(pulse_interval_idx, "data", "interval_index");
+    pulse_event.Set(pulse_interval_trial, "data", "interval_trial");
+    pulse_event.Set(CurrentPulseIntervalMs(), "data", "requested_pulse_interval_ms");
+    pulse_event.Set(CurrentActualPulseIntervalMs(), "data", "actual_pulse_interval_ms");
+    pulse_event.Set(CurrentPairFrequencyHz(), "data", "frequency_Hz");
+    pulse_event.Set(CurrentPairDurationUs(), "data", "duration_us");
     hndl->event_log.Log(pulse_event.Line());
 
     if (status_panel.IsSet()) {
@@ -87,10 +125,25 @@ namespace CML {
     }
     hndl->stim_worker.Stimulate();
     pulse_pair_count++;
+    pulse_interval_trial++;
+
+    if (pulse_interval_trial >= trials_per_interval) {
+      pulse_interval_trial = 0;
+      pulse_interval_idx++;
+
+      if (pulse_interval_idx >= pulse_intervals_ms.size()) {
+        pulse_interval_idx = 0;
+        pulse_interval_repeat++;
+      }
+
+      if (pulse_pair_count < total_pair_count) {
+        ConfigureCurrentInterval();
+      }
+    }
   }
 
   void ExperPulse::RunEvent() {
-    if (next_event_time >= experiment_duration_ms) {
+    if (pulse_pair_count >= total_pair_count) {
       InternalStop();
       return;
     }
--- src/ExperPulse.h
+++ src/ExperPulse.h
@@ -7,6 +7,7 @@
 #include "RC/RStr.h"
 #include "RCqt/Worker.h"
 #include "StimInterface.h"
+#include <array>
 #include <QTimer>
 
 namespace CML {
@@ -43,6 +44,11 @@ namespace CML {
     void Stop_Handler();
     void InternalStop();
     void DoPulsePair();
+    void ConfigureCurrentInterval();
+    uint64_t CurrentPulseIntervalMs() const;
+    uint32_t CurrentPairFrequencyHz() const;
+    uint32_t CurrentPairDurationUs() const;
+    double CurrentActualPulseIntervalMs() const;
 
     protected slots:
     void RunEvent();
@@ -51,18 +57,26 @@ namespace CML {
     void TriggerAt(uint64_t target_ms);
     void BeAllocatedTimer();
 
-    static constexpr uint64_t pulse_interval_ms = 30;
     static constexpr uint64_t pair_period_ms = 1000;
-    static constexpr uint64_t experiment_duration_ms = 5 * 60 * 1000;
-    static constexpr uint32_t pair_frequency_hz = 33;
-    static constexpr uint32_t pair_duration_us = 61000;
+    static constexpr size_t trials_per_interval = 10;
+    static constexpr size_t interval_repeats = 6;
+    static constexpr std::array<uint64_t, 6> pulse_intervals_ms =
+      {5, 15, 30, 50, 100, 200};
+    static constexpr size_t total_pair_count =
+      pulse_intervals_ms.size() * trials_per_interval * interval_repeats;
+    static constexpr uint64_t experiment_duration_ms =
+      total_pair_count * pair_period_ms;
 
     RC::Ptr<Handler> hndl;
     RC::Ptr<StatusPanel> status_panel;
     RC::APtr<QTimer> timer;
+    StimProfile base_stim_profile;
     StimProfile stim_profile;
     uint64_t next_event_time = 0;
     size_t pulse_pair_count = 0;
+    size_t pulse_interval_idx = 0;
+    size_t pulse_interval_trial = 0;
+    size_t pulse_interval_repeat = 0;
     f64 exp_start = 0;
   };
 }
